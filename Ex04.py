ec789167134ea9fe9d329ada0f52c954a63fb8a2
jasonhigh-private <jasonhigh.kim@gmail.com>
2016-11-26 22:05:08 +0900

    Add files via upload

---
+++ Ex04.py
@@ -0,0 +1,16 @@
+# -*- coding: cp949 -*-
+class CounterManager:
+    __insCount = 0
+
+    def __init__(self):
+        CounterManager.__insCount += 1
+
+    def staticPrintCount():
+        print("Instance Count: %d" % CounterManager.__insCount)
+
+    SPrintCount = staticmethod(staticPrintCount)
+
+
+a, b, c = CounterManager(), CounterManager(), CounterManager()
+CounterManager.SPrintCount()
+dir(CounterManager)
